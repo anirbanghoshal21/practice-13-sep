38e643bf26bb54fc40a430e5866546fd205e109d
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-13 23:26:42 +0530

    Part C Prob 1

---
+++ Class3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonOptions
+{
+    public class Class3
+    {
+        public void ChangePassword(int user)
+        {
+            Console.WriteLine("All the user can change password");
+            Console.WriteLine("Every month new password can be set");
+        }
+
+
+    }
+}
