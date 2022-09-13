56b38e251b348c355654ad59685b24fc65581492
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-13 20:24:46 +0530

    Part A prob 3

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Library
+{
+    public class Class1
+    {
+        public void Movie(string language, string genres)
+        {
+            Console.WriteLine("Search different based on language and genre");
+        }
+
+        private string login { get; set; }
+        public void GetLoginCredential()
+        {
+            Console.WriteLine("Login successfully for User");
+        }
+        private string rented { get; set; } 
+        private string password { get; set; }
+
+        public string Rented
+        {
+            get { return rented; }
+            set { rented = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        enum Category
+        {
+            Silver = 2,
+            Gold = 3,
+            Platinum = 5,
+
+        }
+        static void Main(string[] args)
+        {
+            Category rented = Category.Gold;
+            Console.WriteLine(rented);
+        }
+    }
+}
