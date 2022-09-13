233c44632430e82529683413409442268069dca7
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-13 21:27:55 +0530

    Part B Prob 1

---
+++ Class2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminUser
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public void ApplyToCustomer(Customer customer)
+        {
+            customer.MobileNumber = MobileNumber;
+        }
+
+        
+    }
+    
+}
