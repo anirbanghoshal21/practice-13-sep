991014b2d463c769399d6119842668a0aa4abde4
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-13 20:14:19 +0530

    Part A Prob 2

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Library
+{
+    public class Class1
+    {
+        public void Movie(string language, string genres)
+        {
+            Console.WriteLine("Search different based on language and genre");
+        }
+
+        private string login { get; set; }
+        public void GetLoginCredential()
+        {
+            Console.WriteLine("Login successfully for User");
+        }
+        private string rented { get; set; } 
+        private string password { get; set; }
+
+        public string Rented
+        {
+            get { return rented; }
+            set { rented = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
